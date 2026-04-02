8a0bddc3ff3c2d56502861bbfc5bbe9148eb64b2
thalamoosie <thalamoose@protonmail.com>
2026-04-01 22:04:31 -0400

    Implement basic HH:MM real-time clock based on user's locale

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,1 +1,15 @@
-// Dis gonna be a clock obvs
+"use strict";
+
+const systemClock = document.querySelector(".sys-clock");
+console.log("Found element:", systemClock);
+
+const updateClock = function () {
+  const date = new Date();
+  const currentTime = date.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  systemClock.textContent = currentTime;
+};
+
+setInterval(updateClock, 1000);
